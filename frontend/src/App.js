4021922c7881c2b4523563a6240e67ca19dd01da
tacettinkustu <tacettinkustu>
2020-09-11 22:25:10 +0300

    add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,138 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  };
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="header">
+        <div className="brand">
+          <button onClick={openMenu}>&#9776;</button>
+          <a href="index.html">amazona</a>
+        </div>
+        <div className="header-links">
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
       </header>
+      <aside className="sidebar">
+        <h3>Shopping Categories</h3>
+        <button className="sidebar-close-button" onClick={closeMenu}>
+          x
+        </button>
+        <ul>
+          <li>
+            <a href="index.html">Pants</a>
+          </li>
+          <li>
+            <a href="index.html">Shirt</a>
+          </li>
+        </ul>
+      </aside>
+      <main className="main">
+        <div className="content">
+          <ul className="products">
+            <li>
+              <div className="product">
+                <img
+                  className="product-image"
+                  src="/images/d1.jpg"
+                  alt="product"
+                />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">LAcoste</div>
+                <div className="product-price">$50</div>
+                <div className="product-rating">4.2 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img
+                  className="product-image"
+                  src="/images/d1.jpg"
+                  alt="product"
+                />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">LAcoste</div>
+                <div className="product-price">$50</div>
+                <div className="product-rating">4.2 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img
+                  className="product-image"
+                  src="/images/d1.jpg"
+                  alt="product"
+                />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">LAcoste</div>
+                <div className="product-price">$50</div>
+                <div className="product-rating">4.2 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img
+                  className="product-image"
+                  src="/images/d1.jpg"
+                  alt="product"
+                />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">LAcoste</div>
+                <div className="product-price">$50</div>
+                <div className="product-rating">4.2 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img
+                  className="product-image"
+                  src="/images/d1.jpg"
+                  alt="product"
+                />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">LAcoste</div>
+                <div className="product-price">$50</div>
+                <div className="product-rating">4.2 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img
+                  className="product-image"
+                  src="/images/d1.jpg"
+                  alt="product"
+                />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">LAcoste</div>
+                <div className="product-price">$50</div>
+                <div className="product-rating">4.2 Stars (10 Reviews)</div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </main>
+      <footer className="footer">All right reserved.</footer>
     </div>
   );
 }
