2bdf88e7408611b7f3ab278700ee755021d0bcd1
tacettinkustu <tacettinkustu>
2020-09-13 22:29:05 +0300

    add redux to details

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
-import express from 'express';
-import data from './data.js';
+import express from "express";
+import data from "./data.js";
 
 const app = express();
 
+app.get("/api/products/:id", (req, res) => {
+  const productId = req.params.id;
+  const product = data.products.find((x) => x._id === productId);
+  if (product) res.send(product);
+  else res.status(404).send({ msg: "Product Not Found." });
+});
 
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
-app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
+app.listen(5000, () => {
+  console.log("Server started at http://localhost:5000");
+});
