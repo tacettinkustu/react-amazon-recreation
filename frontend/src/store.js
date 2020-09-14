a9ec447d9d5cc72bf8c01c8822dc5270e7818629
tacettinkustu <tacettinkustu>
2020-09-14 19:15:35 +0300

    shopping cart screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,17 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers';
+import Cookie from 'js-cookie';
+import { cartReducer } from './reducers/cartReducers'
+
+const cartItems = Cookie.getJSON("cartItems") || [];
 
 
-const initialState = {};
+const initialState = { cart: { cartItems } };
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
