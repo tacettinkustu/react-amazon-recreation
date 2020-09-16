c9eca3693d17d5fe0d97717c6adf8b99c80b3c02
tacettinkustu <tacettinkustu>
2020-09-16 22:35:07 +0300

    sign in user

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,15 +1,54 @@
-import express from 'express';
-import User from '../models/userModel';
+import express from "express";
+import User from "../models/userModel";
+import { getToken } from "../util";
 
 const router = express.Router();
+
+router.post("/signin", async (req, res) => {
+  const signinUser = await User.findOne({
+    email: req.body.email,
+    password: req.body.password,
+  });
+  if (signinUser) {
+    res.send({
+      _id: signinUser.id,
+      name: signinUser.name,
+      email: signinUser.email,
+      isAdmin: signinUser.isAdmin,
+      token: getToken(signinUser),
+    });
+  } else {
+    res.status(401).send({ msg: "Invalid Email or Password." });
+  }
+});
+
+router.post("/register", async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  const newUser = await user.save();
+  if (newUser) {
+    res.send({
+      _id: newUser.id,
+      name: newUser.name,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+      token: getToken(newUser),
+    });
+  } else {
+    res.status(401).send({ msg: "Invalid User Data." });
+  }
+});
 
 router.get("/createadmin", async (req, res) => {
   try {
     const user = new User({
-      name: 'Basir',
-      email: 'basir.jafarzadeh@gmail.com',
-      password: '1234',
-      isAdmin: true
+      name: "Basir",
+      email: "basir.jafarzadeh@gmail.com",
+      password: "1234",
+      isAdmin: true,
     });
     const newUser = await user.save();
     res.send(newUser);
@@ -18,4 +57,4 @@ router.get("/createadmin", async (req, res) => {
   }
 });
 
-export default router; 
+export default router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,15 @@ import "./App.css";
 import { BrowserRouter, Link, Route } from "react-router-dom";
 import ProductScreen from "./screens/ProductScreen";
 import HomeScreen from "./screens/HomeScreen";
-import CartScreen from './screens/CartScreen';
+import CartScreen from "./screens/CartScreen";
+import SigninScreen from "./screens/SigninScreen";
+import { useSelector } from "react-redux";
+import RegisterScreen from "./screens/RegisterScreen";
 
 function App() {
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+
   const openMenu = () => {
     document.querySelector(".sidebar").classList.add("open");
   };
@@ -24,7 +30,11 @@ function App() {
           </div>
           <div className="header-links">
             <a href="cart.html">Cart</a>
-            <a href="signin.html">Sign In</a>
+            {userInfo ? (
+              <Link to="/profile">{userInfo.name}</Link>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
           </div>
         </header>
         <aside className="sidebar">
@@ -43,6 +53,8 @@ function App() {
         </aside>
         <main className="main">
           <div className="content">
+            <Route path="/signin" component={SigninScreen} />
+            <Route path="/register" component={RegisterScreen} />
             <Route path="/product/:id" component={ProductScreen}></Route>
             <Route path="/cart/:id?" component={CartScreen} />
             <Route path="/" exact={true} component={HomeScreen}></Route>
